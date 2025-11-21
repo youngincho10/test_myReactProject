b0e4430a75aae9ae566f5583cb946b18bd5fcdb3
youngincho10 <young.in.cho10@gmail.com>
2025-11-21 13:56:10 +0900

    homepage 경로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      base: "/test_myReactProject/",
+      base: '/test_myReactProject/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
